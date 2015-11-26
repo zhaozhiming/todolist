afde17c8b3a51d8aeabe63e40e8f73cad068c1a6
zhaozhiming <kingzzm1982@sina.com>
2015-11-26 14:23:02 +0800

    init

---
+++ src/client/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as TodoActions from '../actions/todos'
+
+class App extends Component {
+  render () {
+    return <h1>Hello world</h1>
+  }
+}
+
+function mapStateToProps(state) {
+  return {todos: state.todos};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(TodoActions, dispatch)};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/client/reducers/todos.js
@@ -0,0 +1,6 @@
+const initState = {
+  todos: []
+}
+export default function todos(state=initState, action) {
+  return state;
+}
