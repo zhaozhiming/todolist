e4f0825e0a42fdc72b4cc5809fe8b6fb2d85d8e9
zhaozhiming <kingzzm1982@sina.com>
2015-11-28 16:32:51 +0800

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
-var path = require('path')
-var webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/client/index'
+    './src/client/index',
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/static/'
+    publicPath: '/static/',
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: [ 'babel', 'eslint-loader' ],
       exclude: /node_modules/,
-      include: __dirname
+      include: __dirname,
     }, {
       test: /\.css?$/,
       loaders: [ 'style', 'raw' ],
-      include: __dirname
-    }]
-  }
-}
+      include: __dirname,
+    },
+    ],
+  },
+};
