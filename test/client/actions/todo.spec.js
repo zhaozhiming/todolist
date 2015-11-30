0e94b9b91dec4c3596bf15925efabd2eeca04c83
zhaozhiming <kingzzm1982@sina.com>
2015-11-30 17:07:22 +0800

    add action test

--- test/client/actions/todo.spec.js
+++ test/client/actions/todo.spec.js
@@ -9,4 +9,38 @@ describe('todo actions', () => {
       text: 'Use Redux',
     });
   });
+
+  it('complete todo should create COMPLETE_TODO action', () => {
+    expect(actions.completeTodo(1)).toEqual({
+      type: types.COMPLETE_TODO,
+      id: 1,
+    });
+  });
+
+  it('delete todo should create DELETE_TODO action', () => {
+    expect(actions.deleteTodo(1)).toEqual({
+      type: types.DELETE_TODO,
+      id: 1,
+    });
+  });
+
+  it('complete all should create COMPLETE_ALL action', () => {
+    expect(actions.completeAll()).toEqual({
+      type: types.COMPLETE_ALL,
+    });
+  });
+
+  it('clear completed should create CLEAR_COMPLETED action', () => {
+    expect(actions.clearCompleted()).toEqual({
+      type: types.CLEAR_COMPLETED,
+    });
+  });
+
+  it('update todo should create UPDATE_TODO action', () => {
+    expect(actions.updateTodo(1, 'Use Redux')).toEqual({
+      type: types.UPDATE_TODO,
+      text: 'Use Redux',
+      id: 1,
+    });
+  });
 });
