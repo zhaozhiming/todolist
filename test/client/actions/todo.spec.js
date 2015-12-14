008090a207a6144e0c3811077f2245c12b41b182
zhaozhiming <kingzzm1982@sina.com>
2015-12-14 09:54:13 +0800

    use chai replace expect

--- test/client/actions/todo.spec.js
+++ test/client/actions/todo.spec.js
@@ -1,43 +1,43 @@
-import expect from 'expect';
+import {expect} from 'chai';
 import * as types from '../../../src/client/constants/ActionTypes';
 import * as actions from '../../../src/client/actions/todos';
 
 describe('todo actions', () => {
   it('add todo should create ADD_TODO action', () => {
-    expect(actions.addTodo('Use Redux')).toEqual({
+    expect(actions.addTodo('Use Redux')).to.deep.equal({
       type: types.ADD_TODO,
       text: 'Use Redux',
     });
   });
 
   it('complete todo should create COMPLETE_TODO action', () => {
-    expect(actions.completeTodo(1)).toEqual({
+    expect(actions.completeTodo(1)).to.deep.equal({
       type: types.COMPLETE_TODO,
       id: 1,
     });
   });
 
   it('delete todo should create DELETE_TODO action', () => {
-    expect(actions.deleteTodo(1)).toEqual({
+    expect(actions.deleteTodo(1)).to.deep.equal({
       type: types.DELETE_TODO,
       id: 1,
     });
   });
 
   it('complete all should create COMPLETE_ALL action', () => {
-    expect(actions.completeAll()).toEqual({
+    expect(actions.completeAll()).to.deep.equal({
       type: types.COMPLETE_ALL,
     });
   });
 
   it('clear completed should create CLEAR_COMPLETED action', () => {
-    expect(actions.clearCompleted()).toEqual({
+    expect(actions.clearCompleted()).to.deep.equal({
       type: types.CLEAR_COMPLETED,
     });
   });
 
   it('update todo should create UPDATE_TODO action', () => {
-    expect(actions.updateTodo(1, 'Use Redux')).toEqual({
+    expect(actions.updateTodo(1, 'Use Redux')).to.deep.equal({
       type: types.UPDATE_TODO,
       text: 'Use Redux',
       id: 1,
--- test/client/reducers/todos.spec.js
+++ test/client/reducers/todos.spec.js
@@ -1,34 +1,34 @@
-import expect from 'expect';
+import {expect} from 'chai';
 import todos from '../../../src/client/reducers/todos';
 import { ADD_TODO, COMPLETE_TODO, DELETE_TODO,
   COMPLETE_ALL, CLEAR_COMPLETED, UPDATE_TODO} from '../../../src/client/constants/ActionTypes';
 
 function assertCompleteAll(givenState, expect1Complete, expect2Complete) {
   const state = todos(givenState, {type: COMPLETE_ALL});
-  expect(state[0].completed).toBe(expect1Complete);
-  expect(state[1].completed).toBe(expect2Complete);
+  expect(state[0].completed).to.be.equal(expect1Complete);
+  expect(state[1].completed).to.be.equal(expect2Complete);
 }
 
 describe('components', () => {
   describe('Header', () => {
     it('should add todo correctly', () => {
       const state = todos({}, {type: ADD_TODO, text: 'foo'});
-      expect(state.length).toBe(1);
-      expect(state[0].text).toBe('foo');
-      expect(state[0].completed).toBe(false);
+      expect(state.length).to.be.equal(1);
+      expect(state[0].text).to.be.equal('foo');
+      expect(state[0].completed).to.be.equal(false);
     });
 
     it('should complete todo correctly', () => {
       let state = todos([{completed: false, id: 0}], {type: COMPLETE_TODO, id: 0});
-      expect(state[0].completed).toBe(true);
+      expect(state[0].completed).to.be.equal(true);
       state = todos([{completed: true, id: 0}], {type: COMPLETE_TODO, id: 0});
-      expect(state[0].completed).toBe(false);
+      expect(state[0].completed).to.be.equal(false);
     });
 
     it('should delete todo correctly', () => {
       const state = todos([{id: 0}, {id: 1}], {type: DELETE_TODO, id: 0});
-      expect(state.length).toBe(1);
-      expect(state[0].id).toBe(1);
+      expect(state.length).to.be.equal(1);
+      expect(state[0].id).to.be.equal(1);
     });
 
     it('should complete all correctly', () => {
@@ -39,12 +39,12 @@ describe('components', () => {
 
     it('should clear complete correctly', () => {
       const state = todos([{completed: true}, {completed: false}], {type: CLEAR_COMPLETED});
-      expect(state.length).toBe(1);
+      expect(state.length).to.be.equal(1);
     });
 
     it('should update todo correctly', () => {
       const state = todos([{id: 0, text: 'foo'}], {type: UPDATE_TODO, id: 0, text: 'bar'});
-      expect(state[0].text).toBe('bar');
+      expect(state[0].text).to.be.equal('bar');
     });
   });
 });
